42dcd98f67e06ae230af92df67e538f83fc2f592
JOHNMARK <111337795+JOHNMARK2334@users.noreply.github.com>
2023-08-23 21:44:01 +0300

    Add files via upload

---
+++ Frames.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class MyNew extends JFrame {
+    private JMenuBar bar=new JMenuBar();
+    private JMenu Mnufile,MnuEdit, MnuMore;
+    private JmenuItem  MitNew,MitSave,MitCopy,MitPaste,MitCut,MitExtra;
+
+    
+     
+ public Frames(){
+    super("Menu Frame");
+    Mnufile=new Jmenu("File");
+    MnuEdit =new JMenu("Edit");
+    MnuMore=new JMenu("More");
+    MitNew =new JMenuItem("New");
+    MitSave=new JMenuItem("Save");
+    MitCopy=new JMenuItem("Copy");
+    MitPaste=new JMenuItem("Paste");
+    MitCut=new JMenuItem("Cut");
+    MitExtra=new JMenuItem("Extra");
+    setVisible(true);
+    setSize(300,200);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setJMenuBar(bar);
+    bar.add(MnuFile);
+    bar.add(MnuEdit);
+    bar.add(MnuMore);
+    Mnufile.add(MitNew);
+    MnuFile.add(MitSave);
+    MnuEdit.add(MitCopy);
+    MnuEdit.add(MitPaste);
+    MnuEdit.add(MitCut);
+    MnuFile.add(MnuMore);
+    MnuMore.add(MitExtra);
+ }
+ public static void main(String[] args){
+    new MyNew();
+ }
+    
+}
+
